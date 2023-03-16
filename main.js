56355695590ab4056d26482ede50a425ef7e1c26
What-ever-this-is <yuchestart@gmail.com>
2023-03-15 21:01:29 -0500

    s

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 (()=>{
+function log(){
+
+}
 var devTools=document.createElement("div");
 var navBar=document.createElement("div");
 var navBarTabTemplate = document.createElement("div");
-var navBarTabs=["Elements","Console","Storage","About"];
+var tabs = {
+"Elements":document.createElement("div"),
+"Console":document.createElement("div"),
+"Storage":document.createElement("div"),
+"About":document.createElement("div")
+};
+var keyshortcuts={
+    "control":false,
+    "shift":false,
+    "i":false
+};
+var navBarTabs=["Elements","Console","Storage","About","Hide"];
 var currentTab="Elements";
 var devToolsID=`JSDEVTOOLS-${Math.floor(Math.random()*1000)}-${Math.floor(Math.random()*1000)}-WHATEVERTHISIS`;
 devTools.id=devToolsID;
 devTools.style.width="30%";
+devTools.style.minWidth="400px";
 devTools.style.height="100%";
 devTools.style.position="fixed";
 devTools.style.top="0px";
@@ -14,6 +29,9 @@ devTools.style.right="0px";
 devTools.style.backgroundColor="white";
 devTools.style.fontFamily="sans-serif";
 devTools.style.borderLeft="solid 1px gray";
+devTools.style.zIndex="10000000000";
+devTools.style.display="none";
+devTools.style.pointerEvents="none";
 navBar.style.position="absolute";
 navBar.style.top="0px";
 navBar.style.left="0px";
@@ -31,8 +49,19 @@ var newNavbarTab=navBarTabTemplate.cloneNode(true);
 newNavbarTab.innerText=navBarTabs[i];
 newNavbarTab.onmouseover=function(){this.style.backgroundColor="rgba(255,255,255,0.5)"};
 newNavbarTab.onmouseout=function(){this.style.backgroundColor="rgba(255,255,255,0)"};
-newNavbarTab.onclick=function(){currentTab=this.innerText;};
+newNavbarTab.onclick=function(){currentTab=this.innerText;if(currentTab=="Hide"){currentTab="Elements";devTools.style.display="none";devTools.style.pointerEvents="none";}};
 navBar.appendChild(newNavbarTab);
 }
 document.documentElement.appendChild(devTools);
+document.addEventListener("keydown",function(e){
+keyshortcuts[e.key.toLowerCase()]=true;
+if(keyshortcuts["control"]&&keyshortcuts["shift"]&&keyshortcuts["i"]){
+devTools.style.display="block";
+devTools.style.pointerEvents="all";
+return false;
+}
+});
+document.addEventListener("keyup",function(e){
+keyshortcuts[e.key.toLowerCase()]=false;
+});
 })();
